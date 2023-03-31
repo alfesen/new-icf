900cb9b28b5e8254fc83af64c9016ffaa7a52014
alfesen <89510380+alfesen@users.noreply.github.com>
2023-03-31 15:32:46 +0200

    Add route validation for postHeaderData + implement in HeaderForm

--- backend/routes/header.routes.mjs
+++ backend/routes/header.routes.mjs
@@ -1,6 +1,13 @@
 import { Router } from 'express';
 import { postHeaderData } from '../controllers/header.controllers.mjs';
 import fileUpload from '../middleware/file-upload.mjs';
+import { check } from 'express-validator';
 const router = Router();
-router.post('/:pageTitle/header', fileUpload.fields([{ name: 'desktopImage', maxCount: 1 }, { name: 'mobileImage', maxCount: 1 }]), postHeaderData);
+router.post('/:pageTitle/header', fileUpload.fields([
+    { name: 'desktopImage', maxCount: 1 },
+    { name: 'mobileImage', maxCount: 1 },
+]), [
+    check('pageTitle').notEmpty().isLength({ min: 3, max: 50 }).withMessage('Page Title is required'),
+    check('pageSubtitle').isLength({ min: 3 }).withMessage('Page Title is Required'),
+], postHeaderData);
 export default router;
