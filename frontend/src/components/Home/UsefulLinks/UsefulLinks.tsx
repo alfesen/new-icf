7c8d07e622abbcc09b3249bde8ca9ae8f7075001
alfesen <89510380+alfesen@users.noreply.github.com>
2023-04-18 11:04:19 +0200

    Create and implement UsefulLinks components for Home + styles

---
+++ frontend/src/components/Home/UsefulLinks/UsefulLinks.tsx
@@ -0,0 +1,19 @@
+import s from './UsefulLinks.module.scss'
+import donate from '../../../assets/images/links/donate-sm.jpg'
+import map from '../../../assets/images/links/map-sm.jpg'
+import UsefulLink from '../../UI/Links/UsefulLink/UsefulLink'
+
+const UsefulLinks = () => {
+  return (
+    <div className={s.links}>
+      <UsefulLink
+        url='/about/welcome-location'
+        text='Where we are'
+        image={map}
+      />
+      <UsefulLink url='/giving-to-icf' text='Giving to ICF' image={donate} />
+    </div>
+  )
+}
+
+export default UsefulLinks
