d53e6a77c12d2c1281cdb11d3f19332039bed746
alfesen <89510380+alfesen@users.noreply.github.com>
2023-04-18 15:41:52 +0200

    Create navigation Burger UX component

---
+++ frontend/src/components/UI/UX/Burger/Burger.tsx
@@ -0,0 +1,29 @@
+import { BurgerProps } from '../../../../types/UITypes'
+import s from './Burger.module.scss'
+import { useState, useEffect } from 'react'
+
+const Burger = ({ onToggle }: BurgerProps) => {
+  const [isActive, setIsActive] = useState(false)
+
+  const toggleBurger = () => {
+    setIsActive(is => !is)
+  }
+
+  useEffect(() => {
+    onToggle(isActive)
+  }, [isActive])
+
+  return (
+    <button
+      onClick={toggleBurger}
+      className={`${s.hamburger} ${s['hamburger--spring']} ${
+        isActive ? s['is-active'] : ''
+      }`}>
+      <span className={s['hamburger-box']}>
+        <span className={s['hamburger-inner']}></span>
+      </span>
+    </button>
+  )
+}
+
+export default Burger
