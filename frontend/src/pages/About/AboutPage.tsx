a958c69a1356a4077dd07bfd7d5043f76f13ebff
alfesen <89510380+alfesen@users.noreply.github.com>
2023-04-20 10:25:50 +0200

    Implement SideNavigation for About page

--- frontend/src/pages/About/AboutPage.tsx
+++ frontend/src/pages/About/AboutPage.tsx
@@ -1,6 +1,17 @@
+import { Fragment } from 'react'
+import SideNavigation from '../SideNavigation/SideNavigation'
+import { about } from '../../db/links.json'
+
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <Fragment>
+      <SideNavigation
+        title={about.title}
+        collectionTitle='About'
+        links={about.links}
+      />
+      <div>AboutPage</div>
+    </Fragment>
   )
 }
 
