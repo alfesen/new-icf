de3426385e27d277702553cb902062141c79add1
alfesen <89510380+alfesen@users.noreply.github.com>
2023-04-26 13:14:55 +0200

    Implement location on about welcome page

--- frontend/src/components/About/Location/Location.tsx
+++ frontend/src/components/About/Location/Location.tsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Modal from '../../UI/Modal/Modal'
 import LocationForm from '../LocationForm/LocationForm'
 import Button from '../../UI/Form/Button/Button'
 import s from './Location.module.scss'
+import { useFetchData } from '../../../hooks/useFetchData'
+import { LocationData } from '../../../types/AboutTypes'
+import LoadingSpinner from '../../UI/UX/LoadingSpinner/LoadingSpinner'
 
 const Location = () => {
   const [showModal, setShowModal] = useState<boolean>(false)
+  const [locationData, setLocationData] = useState<LocationData>(null)
+  const { loading, sendRequest } = useFetchData()
+
+  useEffect(() => {
+    const getLocation = async () => {
+      try {
+        const { location } = await sendRequest(
+          'http://localhost:5000/api/about/location'
+        )
+        setLocationData(location)
+      } catch (err) {}
+    }
+    getLocation()
+  }, [])
 
   const showEditModal = () => {
     setShowModal(true)
@@ -28,6 +45,23 @@ const Location = () => {
           Edit
         </Button>
       </div>
+      {loading && <LoadingSpinner />}
+      {!loading && locationData && (
+        <div>
+          <h3>{locationData?.title}</h3>
+          <address>{locationData?.address}</address>
+          <img
+            src={`http://localhost:5000/${locationData?.image}`}
+            alt='Church building'
+          />
+          <p>{locationData?.directions}</p>
+          <iframe
+            src={locationData?.map}
+            width='600'
+            height='450'
+            loading='lazy'></iframe>
+        </div>
+      )}
     </div>
   )
 }
