c41b458d1b71dc89e4028210383fdd12eeee52e4
alfesen <89510380+alfesen@users.noreply.github.com>
2023-05-02 14:02:06 +0200

    Add MemberPage html structure

--- frontend/src/pages/Members/MemberPage.tsx
+++ frontend/src/pages/Members/MemberPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Member } from '../../types/MemberTypes'
 import { useFetchData } from '../../hooks/useFetchData'
+import { convertString } from '../../helpers/convertString'
 
 const MemberPage = () => {
   const [member, setMember] = useState<Member>()
@@ -20,7 +21,35 @@ const MemberPage = () => {
     getMember()
   }, [])
 
-  return <div>Member</div>
+  if (!member) {
+    return <h2>No staff member with provided ID </h2>
+  }
+
+  const { image, name, role, bio, contact } = member
+
+  return (
+    <section>
+      <header>
+        <div>
+          <img src={`http://localhost:5000/${image}`} alt={`${name} avatar`} />
+        </div>
+        <div>
+          <h2>{name}</h2>
+          <h3>{role}</h3>
+        </div>
+      </header>
+      <div>
+        <h3>Bio:</h3>
+        {convertString(bio)}
+      </div>
+      {contact && (
+        <div>
+          <h3>Contact me:</h3>
+          <address>{convertString(contact)}</address>
+        </div>
+      )}
+    </section>
+  )
 }
 
 export default MemberPage
