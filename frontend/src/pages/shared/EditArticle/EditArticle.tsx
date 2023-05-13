7ea4f5e7b73933b975dd90a171eb4d35bde6aedd
alfesen <89510380+alfesen@users.noreply.github.com>
2023-05-13 12:38:13 +0200

    Implement conditional heading rendering relating on params

--- frontend/src/pages/shared/EditArticle/EditArticle.tsx
+++ frontend/src/pages/shared/EditArticle/EditArticle.tsx
@@ -1,10 +1,12 @@
+import { useParams } from 'react-router-dom'
 import ArticleForm from '../../../components/Forms/ArticleForm/ArticleForm'
 import s from './EditArticle.module.scss'
 
 const EditArticle = () => {
+  const {articleId} = useParams()
   return (
     <section className={s.new__article}>
-      <h2>Edit Article</h2>
+      <h2>{articleId ? 'Edit Article' : 'Add Article'}</h2>
       <ArticleForm />
     </section>
   )
