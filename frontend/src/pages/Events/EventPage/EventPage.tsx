96a309893a6be90ceaa4eae11ceef45a1a0cba99
alfesen <89510380+alfesen@users.noreply.github.com>
2023-07-09 20:46:54 +0200

    Implement EventPage fetching logic

--- frontend/src/pages/Events/EventPage/EventPage.tsx
+++ frontend/src/pages/Events/EventPage/EventPage.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useFetchData } from '../../../hooks/useFetchData'
+import { TEvent } from '../../../types/EventsTypes'
+
 const EventPage = () => {
-  return (
-    <div>EventPage</div>
-  )
+  const [event, setEvent] = useState<TEvent>()
+  const { eventId } = useParams()
+  const { sendRequest } = useFetchData()
+
+  useEffect(() => {
+    const getEvent = async () => {
+      try {
+        const { event } = await sendRequest(
+          `http://localhost:5000/api/church-life/events/${eventId}`
+        )
+        setEvent(event)
+      } catch (err) {}
+    }
+    getEvent()
+  }, [])
+
+  return <div>EventPage</div>
 }
 
 export default EventPage
